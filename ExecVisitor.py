d19047483890e16b1f267ab9adc5342737012d62
Chen Xinyang <chenxy1999728@126.com>
2019-12-13 22:39:57 +0800

    Added type notations for visit functions

--- ExecVisitor.py
+++ ExecVisitor.py
@@ -20,21 +20,21 @@ class ExecVisitor(GraphVisitor):
     def setPlotter(self, plotter: Plotter):
         self.plotter = plotter
 
-    def visitStatOrigin(self, ctx):
+    def visitStatOrigin(self, ctx) -> str:
         self.origin_x = self.visit(ctx.expr(0))
         self.origin_y = self.visit(ctx.expr(1))
         return f"Origin: ({self.origin_x}, {self.origin_y})"
 
-    def visitStatScale(self, ctx):
+    def visitStatScale(self, ctx) -> str:
         self.scale_x = self.visit(ctx.expr(0))
         self.scale_y = self.visit(ctx.expr(1))
         return f"Scale: ({self.scale_x}, {self.scale_y})"
 
-    def visitStatRot(self, ctx):
+    def visitStatRot(self, ctx) -> str:
         self.rot = self.visit(ctx.expr())
         return f"Rotation: {self.rot}"
     
-    def visitStatFor(self, ctx):
+    def visitStatFor(self, ctx) -> str:
         start = self.visit(ctx.expr(0))
         end = self.visit(ctx.expr(1))
         step = self.visit(ctx.expr(2))
@@ -63,29 +63,29 @@ class ExecVisitor(GraphVisitor):
 
         return f"Drew points"
 
-    def visitBuiltinColor(self, ctx):
+    def visitBuiltinColor(self, ctx) -> str:
         self.color = ctx.C.text
         return f"Color: {self.color}"
     
-    def visitCustomColor(self, ctx):
+    def visitCustomColor(self, ctx) -> str:
         r = self.visit(ctx.expr(0))
         g = self.visit(ctx.expr(1))
         b = self.visit(ctx.expr(2))
         self.color = (r,g,b)
         return f"Color: {self.color}"
 
-    def visitPowerExpr(self, ctx):
+    def visitPowerExpr(self, ctx) -> float:
         base = self.visit(ctx.expr(0))
         power = self.visit(ctx.expr(1))
         return base**power
     
-    def visitUnaryExpr(self, ctx):
+    def visitUnaryExpr(self, ctx) -> float:
         val = self.visit(ctx.expr())
         if ctx.op.type == GraphParser.MINUS:
             return -val
         return val
 
-    def visitMulDivExpr(self, ctx):
+    def visitMulDivExpr(self, ctx) -> float:
         left = self.visit(ctx.expr(0))
         right = self.visit(ctx.expr(1))
 
@@ -93,7 +93,7 @@ class ExecVisitor(GraphVisitor):
             return left * right
         return left / right
 
-    def visitPlusMinusExpr(self, ctx):
+    def visitPlusMinusExpr(self, ctx) -> float:
         left = self.visit(ctx.expr(0))
         right = self.visit(ctx.expr(1))
 
@@ -101,7 +101,7 @@ class ExecVisitor(GraphVisitor):
             return left + right
         return left - right
     
-    def visitConst(self, ctx):
+    def visitConst(self, ctx) -> float:
         cst = ctx.CONST_ID().getText().lower()
         if cst == 'pi':
             return math.pi
@@ -110,10 +110,10 @@ class ExecVisitor(GraphVisitor):
         else:
             return float(cst)
 
-    def visitVarT(self, ctx):
+    def visitVarT(self, ctx) -> float:
         return self.t
 
-    def visitFuncExpr(self, ctx):
+    def visitFuncExpr(self, ctx) -> float:
         pre_function = self.visit(ctx.expr())
         func = ctx.ID().getText().lower()
         if func == 'sin':
@@ -130,6 +130,6 @@ class ExecVisitor(GraphVisitor):
             return math.sqrt(pre_function)
         raise AttributeError(f"Function '{ctx.ID().getText()}' invalid")
 
-    def visitNestedExpr(self, ctx):
+    def visitNestedExpr(self, ctx) -> float:
         return self.visit(ctx.expr())
 
